cb395c5fc09a035aba503c04bfcc91459b0bacd0
Bojan Knezevic <bojan.knezevic@diwanee.com>
2018-04-20 15:24:23 +0200

    slider finish

---
+++ src/js/slider/slider.js
@@ -0,0 +1,54 @@
+var $ = require('jquery');
+var slick = require('slick-carousel');
+
+module.exports = function() {
+    // HP slider
+
+    var $sliderWrap = $('.c-slider'),
+        $slider = $sliderWrap.find('.c-slider__slides-wrap'),
+        $arrows = $sliderWrap.find('.c-slider__nav'),
+        $arrowLeft = $arrows.find('.left'),
+        $arrowRight = $arrows.find('.right'),
+        windowWidth = $(window).outerWidth();
+
+    function mobileSlider() {
+        $slider.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 600,
+            dots: false,
+            prevArrow: $arrowLeft,
+            nextArrow: $arrowRight
+        });
+    }
+
+    function desktopSlider() {
+        var $thumbs = $sliderWrap.find('.c-slider__thumbs').children();
+
+        $slider.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 400,
+            dots: false,
+            fade: true,
+            arrows: false
+        });
+
+        $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+            $thumbs.removeClass('active');
+            $($thumbs[nextSlide]).addClass('active');
+        });
+
+        $.each($thumbs, function() {
+            $(this).on('click', function() {
+                $slider.slick('goTo', $(this).index());
+            });
+        });
+    }
+
+    windowWidth < 768 ? mobileSlider() : desktopSlider();
+};
--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ var css = require('./scss/style.scss');
 // ----- Require modules here
 var nav = require('./js/nav/nav.js');
 var searchHandler = require('./js/search/search.js');
+var slider = require('./js/slider/slider.js');
 
 // ----- Call modules here
 $(document).ready(function() {
     nav();
     searchHandler();
+    slider();
 });
